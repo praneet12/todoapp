5a7ec5117a8aacb0daca0b8cf547cb46a8017f2c
praneet12 <praneetdrolia@gmail.com>
2017-05-30 12:59:05 +0530

    Todo Application in vuejs

---
+++ src/components/Content.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		hello this is content file
+		<div>
+			<ul>
+				<li v-for="todo in todos" class="todo">
+					<div :class="{active: todo.completed }">
+					{{ todo.text }}
+					 <button v-on:click="complete(todo)">Completed</button>
+					 <button v-on:click="deletetodo(todo.id)">Delete</button>
+
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+/* eslint-disabled */
+export default {
+
+  props: ['todos','deletetodo','complete'],
+  data () {
+    return {
+      msg: 'message part'
+    }
+  }
+}
+</script>
+
+<style>
+	ul {
+		list-style: none;
+	}
+	.todo {
+		padding: 10px;
+    background-color: #6a9de8;
+    border: 1px solid #467fe6;
+    margin-top: -1px;
+    text-align: left;
+	}
+	.active {
+		text-decoration: line-through;
+	}
+</style>
---
+++ src/components/dashboard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="dash">
+    <div> Header Section </div>
+    <div> Aside Section</div>
+    <input v-model="newTodo" />
+    <button v-on:click="addTodo">Add</button>
+    <Content
+        v-bind:todos="todos"
+        v-bind:deletetodo="deletetodo"
+        v-bind:complete="complete"
+    />
+  </div>
+</template>
+
+<script>
+
+import Content from './Content'
+
+export default {
+  methods: {
+      addTodo: function() { if (this.newTodo) this.todos.splice(0,0,{ id: this.todos.length + 1 , text: this.newTodo,completed: false }); console.log(this.todos); this.newTodo = undefined },
+      deletetodo: function(id) {console.log(id); this.index = this.todos.findIndex(x => x.id === id); this.todos.splice(this.index, 1) },
+      complete: function (todo) { todo.completed = true }
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      newTodo : undefined,
+      todos: []
+    }
+  },
+  components: {
+    Content
+  }
+}
+</script>
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
